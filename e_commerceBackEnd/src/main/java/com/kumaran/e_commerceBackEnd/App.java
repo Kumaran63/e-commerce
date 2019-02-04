53e400e3f030e46e15afbdf8fc59f7e576d350e3
Kumaran63 <Kumaran63@users.noreply.github.com>
2019-02-04 21:34:27 +0530

    My project Setup over.

---
+++ e_commerceBackEnd/src/main/java/com/kumaran/e_commerceBackEnd/App.java
@@ -0,0 +1,13 @@
+package com.kumaran.e_commerceBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
